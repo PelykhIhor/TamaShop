ea6352443619f26f6815127e5aadab78ecd585cd
PelykhIhor <159559700+PelykhIhor@users.noreply.github.com>
2024-09-15 23:12:58 +0300

    Update about-us.js

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -22,7 +22,7 @@ export default function AboutUS() {
                         <a href="#">Ігор Євгенійович</a>
                     </h3>
                     <span className="text-gray-500 dark:text-gray-400">Засновник інтернет-магазину</span>
-                    <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Тут короткий опис про людину</p>
+                    <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Розробник інтернет-магазину</p>
                     <ul className="flex space-x-4 sm:mt-0">
                         <li>
                             <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
