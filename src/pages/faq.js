d62b614a5c806ea802395f9e9e67d5370c02f420
PelykhIhor <159559700+PelykhIhor@users.noreply.github.com>
2024-09-16 18:23:11 +0300

    Update faq.js

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,8 +1,10 @@
 import Layout from '@/components/Layout';
 import React from 'react';
+import Image from 'next/image';
+
 
 const faqData = [
-  {
+    {
         id: "benefits",
         question: "Наші переваги",
         answer: "Ми пропонуємо преміум техніку Apple з професійним налаштуванням та індивідуальним підходом. Кожен пристрій проходить ретельну перевірку нашими досвідченими фахівцями, щоб гарантувати його бездоганну роботу на найвищому рівні. Також ми берем на себе відповідальність виконати перше налаштування вашої нової техніки, допомагаючи вам швидко та легко почати користуватися всіма можливостями вашого пристрою Apple. Особлива перевага нашого магазину — унікальна послуга розширеної гарантії лише за 500 грн на рік. Ця гарантія включає повне налаштування всіх необхідних додатків, оптимізацію роботи пристрою та вирішення будь-яких проблем з прошивкою. З нами ваша техніка Apple завжди працюватиме як годинник, а ви отримаєте повну підтримку та супровід.",
@@ -20,68 +22,56 @@ const faqData = [
 ];
 
 export default function FAQ() {
-  return (
-    <Layout>
-      <section className="py-24 bg-white dark:bg-gray-900">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="mb-16 text-center">
-            <h2 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Часті запитання
-            </h2>
-          </div>
-          <div className="accordion-group">
-            {faqData.map((faq, index) => (
-              <AccordionItem key={index} id={faq.id} question={faq.question} answer={faq.answer} />
-            ))}
-          </div>
-        </div>
-      </section>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <section className="bg-white dark:bg-gray-900 m-5 rounded-3xl shadow">
+                <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+                    <h2 className="mb-8 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+                        Часті запитання
+                    </h2>
+                    <div className="grid pt-8 text-left border-t border-gray-200 dark:border-gray-700 md:gap-16 md:grid-cols-1">
+                        {faqData.map((faq, index) => (
+                            <FAQItem key={index} id={faq.id} question={faq.question} answer={faq.answer} />
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </Layout>
+    );
 }
 
-function AccordionItem({ id, question, answer }) {
-  return (
-    <div
-      className="accordion-item border border-solid border-gray-300 rounded-xl mb-4 p-4 transition-all duration-500 bg-white dark:bg-gray-800 dark:border-gray-600"
-      id={id}
-    >
-      <button
-        className="accordion-toggle group w-full text-left flex justify-between items-center text-lg font-medium text-gray-900 dark:text-white"
-        aria-controls={`collapse-${id}`}
-        aria-expanded="false"
-      >
-        <h5>{question}</h5>
-        <span className="transition-transform duration-300 group-hover:rotate-90">
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
+function FAQItem({ id, question, answer }) {
+    return (
+        <div id={id} className="mb-10">
+            <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                <Icon />
+                {question}
+            </h3>
+            <p className="text-gray-500 dark:text-gray-400">
+                {answer.split('\n').map((line, index) => (
+                    <span key={index}>
+                        {line}
+                        <br />
+                    </span>
+                ))}
+            </p>
+        </div>
+    );
+}
+
+function Icon() {
+    return (
+        <svg
+            className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
-          >
+        >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M12 4v16m8-8H4"
+                fillRule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                clipRule="evenodd"
             />
-          </svg>
-        </span>
-      </button>
-      <div
-        id={`collapse-${id}`}
-        className="accordion-content mt-4 overflow-hidden max-h-0 transition-all duration-500"
-      >
-        <p className="text-gray-700 dark:text-gray-400">
-          {answer.split('\n').map((line, index) => (
-            <span key={index}>
-              {line}
-              <br />
-            </span>
-          ))}
-        </p>
-      </div>
-    </div>
-  );
+        </svg>
+    );
 }
