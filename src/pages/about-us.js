541a06bab811788062504a7b40b95cd88a78522b
PelykhIhor <159559700+PelykhIhor@users.noreply.github.com>
2024-09-16 17:37:36 +0300

    Update about-us.js

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,76 +1,108 @@
 import Layout from '@/components/Layout';
 import React from 'react';
+import Image from 'next/image';
 
-export default function AboutUs() {
+export default function AboutUS() {
     return (
         <Layout>
-            <section className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 dark:bg-gray-900 text-white">
-                <div className="py-12 px-6 mx-auto max-w-screen-xl lg:py-20 lg:px-8">
-                    <div className="mx-auto max-w-screen-sm text-center mb-12 lg:mb-20">
-                        <h2 className="mb-6 text-5xl font-extrabold tracking-tight leading-tight text-white">Про TamaShop</h2>
-                        <p className="text-lg font-medium text-gray-200">
-                            TamaShop - ваш надійний партнер у світі техніки Apple з 2020 року.
+            <section className="bg-white dark:bg-gray-900">
+                <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                    <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Про Нас</h2>
+                        <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
+                            У TamaShop ми пропонуємо найкращу техніку Apple для вашого комфорту та продуктивності. 
                         </p>
                     </div>
 
-                    <div className="grid gap-10 lg:gap-16 lg:grid-cols-2 mb-12 lg:mb-20">
-                        <div className="relative bg-white rounded-xl shadow-lg transform hover:scale-105 transition duration-300">
-                            <a href="#">
-                                <img className="w-full rounded-t-xl" src="https://i.postimg.cc/SjPs6GTV/wallpaperflare-com-wallpaper-2.jpg" alt="Член команди 1" />
-                            </a>
-                            <div className="p-6">
-                                <h3 className="text-2xl font-semibold text-gray-900">
-                                    <a href="#">Ігор Євгенійович</a>
+                    {/* Відповіді на П'ять питань */}
+                    <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+
+                        {/* Хто? */}
+                        <div className="bg-gray-50 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-5 text-center">
+                                <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Хто Ми?
                                 </h3>
-                                <span className="block text-gray-500">Засновник інтернет-магазину</span>
-                                <p className="mt-4 text-gray-600">Розробник інтернет-магазину</p>
-                                <div className="flex space-x-4 mt-6">
-                                    <a href="#" className="text-gray-500 hover:text-gray-700 transition duration-150">
-                                        <svg className="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <path d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z"/>
-                                        </svg>
-                                    </a>
-                                    <a href="#" className="text-gray-500 hover:text-gray-700 transition duration-150">
-                                        <svg className="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <path d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168Z"/>
-                                        </svg>
-                                    </a>
-                                    <a href="#" className="text-gray-500 hover:text-gray-700 transition duration-150">
-                                        <svg className="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <path d="M22 5.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.343 8.343 0 0 1-2.605.981A4.13 4.13 0 0 0 15.85 4a4.068 4.068 0 0 0-4.1 4.038c0 .31.035.618.105.919A11.705 11.705 0 0 1 3.4 4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 6.1 13.635a4.192 4.192 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 2 18.184 11.732 11.732 0 0 0 8.291 20 11.502 11.502 0 0 0 19.964 8.5"/>
-                                        </svg>
-                                    </a>
-                                </div>
+                                <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">
+                                    Ми TamaShop — експерти в технологіях Apple. Наша команда складається з професіоналів, що готові допомогти вам знайти найкраще рішення для будь-якої потреби.
+                                </p>
                             </div>
                         </div>
 
-                        <div className="relative bg-white rounded-xl shadow-lg transform hover:scale-105 transition duration-300">
-                            <a href="#">
-                                <img className="w-full rounded-t-xl" src="https://i.postimg.cc/hhvvQYrf/wallpaperflare-com-wallpaper-1.jpg" alt="Член команди 2" />
-                            </a>
-                            <div className="p-6">
-                                <h3 className="text-2xl font-semibold text-gray-900">
-                                    <a href="#">Яна Артемівна</a>
+                        {/* Що? */}
+                        <div className="bg-gray-50 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-5 text-center">
+                                <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Що Ми Робимо?
                                 </h3>
-                                <span className="block text-gray-500">Співзасновник інтернет-магазину</span>
-                                <p className="mt-4 text-gray-600">Дизайнер❤️</p>
-                                <div className="flex space-x-4 mt-6">
-                                    <a href="#" className="text-gray-500 hover:text-gray-700 transition duration-150">
-                                        <svg className="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <path d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591Z"/>
-                                        </svg>
-                                    </a>
-                                    <a href="#" className="text-gray-500 hover:text-gray-700 transition duration-150">
-                                        <svg className="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <path d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168Z"/>
-                                        </svg>
-                                    </a>
-                                    <a href="#" className="text-gray-500 hover:text-gray-700 transition duration-150">
-                                        <svg className="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <path d="M22 5.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.343 8.343 0 0 1-2.605.981A4.13 4.13 0 0 0 15.85 4a4.068 4.068 0 0 0-4.1 4.038c0 .31.035.618.105.919A11.705 11.705 0 0 1 3.4 4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 6.1 13.635a4.192 4.192 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 2 18.184 11.732 11.732 0 0 0 8.291 20 11.502 11.502 0 0 0 19.964 8.5"/>
-                                        </svg>
-                                    </a>
-                                </div>
+                                <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">
+                                    Ми спеціалізуємось на продажу техніки Apple, надаючи клієнтам найсучасніші та надійні продукти, такі як iPhone, MacBook та інші аксесуари.
+                                </p>
+                            </div>
+                        </div>
+
+                        {/* Де? */}
+                        <div className="bg-gray-50 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-5 text-center">
+                                <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Де Нас Знайти?
+                                </h3>
+                                <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">
+                                    Ми знаходимося в Києві. Ви можете відвідати наш офіс або замовити онлайн доставку по всій Україні.
+                                </p>
+                            </div>
+                        </div>
+
+                        {/* Коли? */}
+                        <div className="bg-gray-50 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-5 text-center">
+                                <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Коли Ми Працюємо?
+                                </h3>
+                                <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">
+                                    Наші робочі години: з понеділка по п’ятницю з 9:00 до 18:00.
+                                </p>
+                            </div>
+                        </div>
+
+                        {/* Чому? */}
+                        <div className="bg-gray-50 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-5 text-center">
+                                <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Чому Обрати Нас?
+                                </h3>
+                                <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">
+                                    Ми пропонуємо тільки оригінальні продукти Apple з гарантією якості. Наша команда готова допомогти вам з усіма питаннями, пов’язаними з вибором техніки.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Команда */}
+                    <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+                        <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+                            <div>
+                                <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://i.postimg.cc/SjPs6GTV/wallpaperflare-com-wallpaper-2.jpg" alt="Член команди 1" />
+                            </div>
+                            <div className="p-5">
+                                <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Ігор Євгенійович
+                                </h3>
+                                <span className="text-gray-500 dark:text-gray-400">Засновник інтернет-магазину</span>
+                                <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Розробник інтернет-магазину</p>
+                            </div>
+                        </div>
+
+                        <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+                            <div>
+                                <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://i.postimg.cc/hhvvQYrf/wallpaperflare-com-wallpaper-1.jpg" alt="Член команди 2" />
+                            </div>
+                            <div className="p-5">
+                                <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Яна Артемівна
+                                </h3>
+                                <span className="text-gray-500 dark:text-gray-400">Співзасновник інтернет-магазину</span>
+                                <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Дизайнер ❤️</p>
                             </div>
                         </div>
                     </div>
