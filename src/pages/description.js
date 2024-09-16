163d4123484b7d6a359e9c8a9dbac189e8365bd4
PelykhIhor <159559700+PelykhIhor@users.noreply.github.com>
2024-09-16 14:10:14 +0300

    Update description.js

--- src/pages/description.js
+++ src/pages/description.js
@@ -66,7 +66,7 @@ export default function Description() {
                             <div className="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
                                 <a
                                     href="#"
-                                    className="flex items-center justify-center py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                    className="flex items-center justify-center py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-purple-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                                 >
                                     <svg
                                         className="w-5 h-5 -ms-2 me-2"
@@ -88,7 +88,7 @@ export default function Description() {
 
                                 <button
                                     onClick={handleAddToCart}
-                                    className="text-white mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
+                                    className="text-white mt-4 sm:mt-0 bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800 flex items-center justify-center"
                                 >
                                     <svg
                                         className="w-5 h-5 -ms-2 me-2"
