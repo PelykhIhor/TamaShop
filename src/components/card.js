9b5e3b64fab2ab4623088e48c529cea89007ae4e
PelykhIhor <159559700+PelykhIhor@users.noreply.github.com>
2024-09-16 14:14:48 +0300

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -78,7 +78,7 @@ const ProductCard = ({ image, image2, title, price, rating, discount, delivery, 
                 </ul>
                 <div className="mt-4 flex items-center justify-between gap-4">
                     <p className="text-2xl font-extrabold leading-tight text-gray-900 dark:text-white">${price}</p>
-                    <button onClick={handleBuyClick} type="button" className="inline-flex items-center rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800 transform transition duration-500 hover:scale-105">
+                    <button onClick={handleClick} type="button" className="inline-flex items-center rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800 transform transition duration-500 hover:scale-105">
                         <svg className="-ms-2 me-2 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
                         </svg>
